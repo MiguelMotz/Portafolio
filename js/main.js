dddaeac18745edcaff98a5c56c561cc889ea5010
Motz <bmxmotz@gmail.com>
2023-07-05 17:30:29 -0500

    Preguntas

--- js/main.js
+++ js/main.js
@@ -15,7 +15,19 @@
     });
 
     const changePosition = (add)=>{
-        console.log(add);
+        const currentTestimony = document.querySelector('.testimony_body--show').dataset.id;
+        value = Number (currentTestimony);
+        value+= add;
+
+       
+        sliders[Number(currentTestimony)-1].classList.remove('testimony_body--show');
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length : 1;
+        }
+        
+        sliders[value-1].classList.add('testimony_body--show');
+
+
     } 
-    console.log (buttonNext)
+     
 })();
